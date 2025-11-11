05faaeea3226e230de3ff351211c4d0cd3b48151
Martijn Voncken <mvoncken@gmail.com>
2025-11-11 12:03:34 +0100

    Conforming to owlbear light/dark style. and style switching

--- owl20.js
+++ owl20.js
@@ -1,6 +1,6 @@
 import OBR from "@owlbear-rodeo/sdk";
 
-const sound = new Audio('dice-89594.mp3');
+
 
 const renderRoll = (html) =>{
     const element = document.querySelector("#rolls-list");
@@ -11,6 +11,8 @@ const renderRoll = (html) =>{
 }
 
 const broadcastRoll = (request) =>{
+    // load sound every time because I want to play 5 at a time if someone 5-clicked.
+    const sound = new Audio('dice-89594.mp3');
     sound.load();
     sound.play();      
     const html = `<h3>${request.character}</h3> ${request.html}`;
@@ -29,11 +31,29 @@ addEventListener("message", (event) => {
   }
 })
 
+
+
+
+const themeManager  = async () => {
+  const setTheme = (theme) => {
+      console.log(theme)
+      document.body.setAttribute('data-theme',theme.mode);
+  }
+  let theme = await OBR.theme.getTheme();
+  setTheme(theme)
+  OBR.theme.onChange((theme) => {
+    setTheme(theme)
+  })
+}
+
+
+  
 OBR.onReady(() => {
   console.log("owl20-owlbear: OBR Ready")
   OBR.broadcast.onMessage("owl20.roll", (event) => {
       renderRoll(event.data); 
   })
+  themeManager()
 })
 
 window.OBR = OBR

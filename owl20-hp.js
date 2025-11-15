baf8ac36098344b8a81714660ca8f5b13a0b032a
Martijn Voncken <mvoncken@gmail.com>
2025-11-15 07:43:48 +0100

    add HP broadcast, tweak styles

--- owl20-hp.js
+++ owl20-hp.js
@@ -1,60 +1,16 @@
 import OBR from "@owlbear-rodeo/sdk";
 import he from 'he';
 import "./style.css";
+import { themeManager } from "./theme";
 
 const renderHP = (request) =>{
-    let  html = `<h3><span title="${he.encode(request.playerName)}" class=playerBubble style="background-color:${request.playerColor}"></span>${he.encode(request.character)}</h3> ${request.html}`;
-  
     /*
-    compromise for now, firure it out
-    if (request.request) {
-      html = `<h3><img src="${request.request.character.avatar}" class="playerAvatar"></span>${he.encode(request.character)}</h3> ${request.html}`;
-    }*/
-
-
-    console.log(request.request.character);
-
-    const element = document.querySelector("#rolls-list");
-    const node = document.createElement("div");
-    node.innerHTML = html;
-    element.appendChild(node); 
-    window.scrollTo(0, document.body.scrollHeight);
-}
-
-const broadcastHP = async (request) => {
-    // load sound every time because I want to play 5 at a time if someone 5-clicked.    
-    request.playerColor = await OBR.player.getColor();
-    request.playerName = await OBR.player.getName();
-
-    OBR.broadcast.sendMessage("owl20.hp", request, {destination:"ALL"});
-}
-
-addEventListener("message", (event) => { 
-  //broadcastRoll(event) 
-  switch(event.data.type) {
-    case 'Beyond20_Roll':
-      console.log("owl20-owlbear: Broadcasting roll:", event.data.data);
-      broadcastRoll(event.data.data);
-      break;
-    default:
-      //Ignore unknown messages silently
-  }
-})
-
-const themeManager  = async () => {
-  const setTheme = (theme) => {
-      console.log(theme)
-      document.body.setAttribute('data-theme',theme.mode);
-  }
-  let theme = await OBR.theme.getTheme();
-  setTheme(theme)
-  OBR.theme.onChange((theme) => {
-    setTheme(theme)
-  })
+    what do i even want to render?
+    do i need a framework/library?
+    */
 }
 
 OBR.onReady(() => {
-  console.log("owl20-owlbear: OBR Ready")
   OBR.broadcast.onMessage("owl20.hp", (event) => {
       renderHP(event.data); 
   })
--- owl20.js
+++ owl20.js
@@ -1,62 +1,60 @@
 import OBR from "@owlbear-rodeo/sdk";
 import he from 'he';
-
+import { themeManager } from "./theme";
 import "./style.css";
 
+let prevChar = "prev"
+
 const renderRoll = (request) =>{
-    let  html = `<h3><span title="${he.encode(request.playerName)}" class=playerBubble style="background-color:${request.playerColor}"></span>${he.encode(request.character)}</h3> ${request.html}`;
-  
-    /*
-    compromise for now, firure it out
-    if (request.request) {
-      html = `<h3><img src="${request.request.character.avatar}" class="playerAvatar"></span>${he.encode(request.character)}</h3> ${request.html}`;
-    }*/
-
-
-    console.log(request.request.character);
+    let html = '';
+    const char = `${request.character}-${request.playerName}`;
+    if (char !== prevChar) {
+      // only show the character/player header if it changes.
+      html = `<h3><span title="${he.encode(request.playerName)}" class=playerBubble style="background-color:${request.playerColor}"></span>${he.encode(request.character)}</h3>`
+    }
+    html += request.html;
 
     const element = document.querySelector("#rolls-list");
     const node = document.createElement("div");
     node.innerHTML = html;
     element.appendChild(node); 
     window.scrollTo(0, document.body.scrollHeight);
+    prevChar = char;
 }
 
 const broadcastRoll = async (request) => {
-    // load sound every time because I want to play 5 at a time if someone 5-clicked.
+    // load sound every time because I want to play 3 at a time if someone 3-clicked.
     const sound = new Audio('dice-89594.mp3');
     sound.load();
     sound.play();      
-
     request.playerColor = await OBR.player.getColor();
     request.playerName = await OBR.player.getName();
 
     OBR.broadcast.sendMessage("owl20.roll", request, {destination:"ALL"});
 }
 
+const broadcastHP = async (request) => {
+    // This broadast can be picked up by other extensions.
+    request.playerColor = await OBR.player.getColor();
+    request.playerName = await OBR.player.getName();
+
+    OBR.broadcast.sendMessage("owl20.hp", request, {destination:"ALL"});
+}
+
+
 addEventListener("message", (event) => { 
   //broadcastRoll(event) 
   switch(event.data.type) {
     case 'Beyond20_Roll':
-      console.log("owl20-owlbear: Broadcasting roll:", event.data.data);
       broadcastRoll(event.data.data);
       break;
+    case 'Beyond20_UpdateHP':
+      broadcastHP(event.data.data);
+      break;      
     default:
       //Ignore unknown messages silently
   }
 })
-
-const themeManager  = async () => {
-  const setTheme = (theme) => {
-      console.log(theme)
-      document.body.setAttribute('data-theme',theme.mode);
-  }
-  let theme = await OBR.theme.getTheme();
-  setTheme(theme)
-  OBR.theme.onChange((theme) => {
-    setTheme(theme)
-  })
-}
 
 OBR.onReady(() => {
   console.log("owl20-owlbear: OBR Ready")
@@ -64,6 +62,8 @@ OBR.onReady(() => {
       renderRoll(event.data); 
   })
   themeManager()
+  // sa-event : room is unique enough, but non identifyable.
+  window.sa_event("room",{ slug: OBR.room.id.substring(0,4)});
 })
 
 window.OBR = OBR
